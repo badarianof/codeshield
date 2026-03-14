73c8f0854e194b92251823ba79bf76cec19013fe
Merrone <200412392+strxwberrylxvver@users.noreply.github.com>
2026-03-14 19:25:25 +0000

    Run Scan functionality fully implemented to calculate CC

--- static/js/script.js
+++ static/js/script.js
@@ -17,3 +17,41 @@ document.querySelector(".btn-run").addEventListener("click", async function () {
     alert("Please select a .py file first.");
     return;
   }
+
+  const source = await file.text();
+
+  const response = await fetch("/scan", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ source: source, filename: file.name }),
+  });
+
+  const data = await response.json();
+
+  showResults(data);
+});
+
+function showResults(data) {
+  let html = "<h2>Results for: " + data.filename + "</h2>";
+  html += "<table border='1' cellpadding='8'>";
+  html += "<tr><th>Function</th><th>Complexity</th><th>Risk</th></tr>";
+
+  for (const fn of data.functions) {
+    html += "<tr>";
+    html += "<td>" + fn.function + "</td>";
+    html += "<td>" + fn.complexity + "</td>";
+    html += "<td>" + fn.risk + "</td>";
+    html += "</tr>";
+  }
+
+  html += "</table>";
+
+  let resultsDiv = document.getElementById("results");
+  if (!resultsDiv) {
+    resultsDiv = document.createElement("div");
+    resultsDiv.id = "results";
+    document.querySelector("main").appendChild(resultsDiv);
+  }
+
+  resultsDiv.innerHTML = html;
+}
